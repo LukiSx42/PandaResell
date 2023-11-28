13ffe7bd57d451c9e04f11d001b628ba57f25778
LukiSx420 <lukas.oravec@live.com>
2023-11-28 11:13:45 +0100

    Exception prevention

--- scraper/src/yupoo.py
+++ scraper/src/yupoo.py
@@ -144,152 +144,155 @@ class YupooScraper:
         parsed = []
         
         for item in scraped:
-            if item["id"] in self.db["known"]:
-                print("[Y] Item {} is already in our database!".format(item["id"]))
-                parsed.append(self.db["items"][item["id"]])
-                continue
-            self.driver.get(item["link"])
-            s = BeautifulSoup(self.driver.page_source, 'html.parser')
-            
-            name = s.find_all('span', { "class": "showalbumheader__gallerytitle" })[0].text
-            w2c = s.find_all('div', { "class": "showalbumheader__gallerysubtitle" })[0].find_all('a')[0].text
-            iconID = s.find_all('img', { "class": "autocover" })[0]['src'].split("/")[-2]
-
-            images = self.driver.find_elements(By.CLASS_NAME, "image__imagewrap")
-            c = 1
-
-            images[0].click()
-            time.sleep(self.cfg["options"]["image_load_time"])
-            while True:
-                nextImg = self.driver.find_element(By.CLASS_NAME, "viewer__next")
-                if not nextImg:
-                    break
-                
-                viewer = self.driver.find_element(By.CLASS_NAME, "viewer__img")
-
-                loadts = time.time()
-                while "viewer__loading" in viewer.get_attribute("class").split(" "): # Wait for the image to load
-                    time.sleep(self.cfg["options"]["image_load_time"])
-                    viewer = self.driver.find_element(By.CLASS_NAME, "viewer__img")
-                    if loadts+self.cfg["options"]["image_load_timeout"] < time.time(): # Load timeout
-                        self.driver.back()
-                        print("Image load timeout exceeded {} seconds".format(self.cfg["options"]["image_load_timeout"]))
-                        break
-                if loadts+self.cfg["options"]["image_load_timeout"] < time.time():
+            try:
+                if item["id"] in self.db["known"]:
+                    print("[Y] Item {} is already in our database!".format(item["id"]))
+                    parsed.append(self.db["items"][item["id"]])
                     continue
-                if "displaynone" in viewer.get_attribute("class").split(" "): # If its a video, skip it
+                self.driver.get(item["link"])
+                s = BeautifulSoup(self.driver.page_source, 'html.parser')
+                
+                name = s.find_all('span', { "class": "showalbumheader__gallerytitle" })[0].text
+                w2c = s.find_all('div', { "class": "showalbumheader__gallerysubtitle" })[0].find_all('a')[0].text
+                iconID = s.find_all('img', { "class": "autocover" })[0]['src'].split("/")[-2]
+
+                images = self.driver.find_elements(By.CLASS_NAME, "image__imagewrap")
+                c = 1
+
+                images[0].click()
+                time.sleep(self.cfg["options"]["image_load_time"])
+                while True:
+                    nextImg = self.driver.find_element(By.CLASS_NAME, "viewer__next")
+                    if not nextImg:
+                        break
+                    
+                    viewer = self.driver.find_element(By.CLASS_NAME, "viewer__img")
+
+                    loadts = time.time()
+                    while "viewer__loading" in viewer.get_attribute("class").split(" "): # Wait for the image to load
+                        time.sleep(self.cfg["options"]["image_load_time"])
+                        viewer = self.driver.find_element(By.CLASS_NAME, "viewer__img")
+                        if loadts+self.cfg["options"]["image_load_timeout"] < time.time(): # Load timeout
+                            self.driver.back()
+                            print("Image load timeout exceeded {} seconds".format(self.cfg["options"]["image_load_timeout"]))
+                            break
+                    if loadts+self.cfg["options"]["image_load_timeout"] < time.time():
+                        continue
+                    if "displaynone" in viewer.get_attribute("class").split(" "): # If its a video, skip it
+                        try:
+                            nextImg.click()
+                        except:
+                            break
+                        time.sleep(self.cfg["options"]["image_load_time"])
+                        continue
+
+                    if viewer.get_attribute("src").split("/")[-2] == iconID: # If the image is used as an icon
+                        viewer.screenshot(item["iconPath"])
+                    viewer.screenshot(item["imagePath"]+"/"+str(c)+".png")
+
                     try:
                         nextImg.click()
                     except:
                         break
                     time.sleep(self.cfg["options"]["image_load_time"])
-                    continue
+                    c += 1
+                
+                i = { # FINAL ITEM SCRTUCTURE
+                    "name": "",
+                    "desc": "",
+                    "price": [], # (Min value, Max value)
+                    "basePrice": {
+                        "yuan": [],
+                        "euro": []
+                    },
+                    "brand": [],
+                    "type": [],
+                    "w2c": w2c,
+                    "id": item["id"],
+                    "seller": seller["name"].lower().replace(" ", "_")
+                }
+                
+                if "items" in category.keys(): # Applying prespecified info
+                    if "type" in category["items"].keys():
+                        if type(category["items"]["type"]) is str:
+                            i["type"] = [ category["items"]["type"] ]
+                        else:
+                            i["type"] = category["items"]["type"]
+                    if "brand" in category["items"].keys():
+                        i["brand"] = category["items"]["brand"]
 
-                if viewer.get_attribute("src").split("/")[-2] == iconID: # If the image is used as an icon
-                    viewer.screenshot(item["iconPath"])
-                viewer.screenshot(item["imagePath"]+"/"+str(c)+".png")
+                if "husky" in seller["name"] or True: # DEFAULT NAME PARSING ROUTE
+                    # SKIPPING ITEMS WE CANNOT PARSE
+                    nums = re.findall(r'\d+', name)
+                    if len(nums) == 0:
+                        print("[Y] ! Warning: Didn't find any numbers in item named '{}'\n >> Skipping item...".format(name))
+                        continue
+                    if "￥" not in name:
+                        print("[Y] ! Warning: Didn't find YUAN in item named '{}'\n >> Skipping item...".format(name))
+                        continue
+                    if name.count("￥") > len(nums):
+                        print("[Y] ! Warning: There are {} prices in item named '{}' but only {} numbers\n >> Skipping item...".format(name.count("￥"), name, len(nums)))
+                        continue
+                    
+                    # NAME PARSING
+                    i["name"] = name.split("（")[0].split("(")[0].replace(".", "").replace("~", "").replace(",", "")
+                    i["name"] = i["name"][i["name"].index(str(nums[0]))+len(str(nums[0])):]
 
-                try:
-                    nextImg.click()
-                except:
-                    break
-                time.sleep(self.cfg["options"]["image_load_time"])
-                c += 1
-            
-            i = { # FINAL ITEM SCRTUCTURE
-                "name": "",
-                "desc": "",
-                "price": [], # (Min value, Max value)
-                "basePrice": {
-                    "yuan": [],
-                    "euro": []
-                },
-                "brand": [],
-                "type": [],
-                "w2c": w2c,
-                "id": item["id"],
-                "seller": seller["name"].lower().replace(" ", "_")
-            }
-            
-            if "items" in category.keys(): # Applying prespecified info
-                if "type" in category["items"].keys():
-                    if type(category["items"]["type"]) is str:
-                        i["type"] = [ category["items"]["type"] ]
+                    # DESCRIPTION PARSING
+                    if "（" in name:
+                        i["desc"] = name.split("（")[1].replace("）", "")
+                    elif "(" in name:
+                        i["desc"] = name.split("(")[1].replace(")", "")
                     else:
-                        i["type"] = category["items"]["type"]
-                if "brand" in category["items"].keys():
-                    i["brand"] = category["items"]["brand"]
+                        i["desc"] = None
+                    if i["desc"] is not None:
+                        if i["desc"][-4:] == "phot":
+                            i["desc"] += "o"
+                    
+                    # BRAND DECODING + PARSING
+                    foundBrands = []
+                    for brand in list(self.brands.keys()):
+                        if brand in i["name"]: # Brand found in decoding table
+                            foundBrands.append(brand)
+                            i["brand"].append(self.brands[brand])
+                            i["name"] = i["name"].replace(brand, "") # Remove the brand from the item name
+                    
+                    if len(foundBrands) < i["name"].upper().count(" X ") + 1: # If there were brands that were not found in the decoding table
+                        for namePart in i["name"].split(" X "):
+                            for idx in range(len(namePart)):
+                                if namePart[idx].isalpha():
+                                    break
+                            if len(namePart) > 0:
+                                namePart = namePart[idx:].split(" ")[0]
+                                namePart = namePart[0].upper() + namePart[1:].lower()
+                                i["brand"].append(namePart)
+                    
+                    # NAME PARSING - REMOVING SPACES AFTER BRAND DECODING
+                    while i["name"][0] == " ": # Remove additional spaces at the start
+                        i["name"] = i["name"][1:]
+                    while i["name"][-1] == " ": # Remove additional spaces at the end
+                        i["name"] = i["name"][:-1]
 
-            if "husky" in seller["name"] or True: # DEFAULT NAME PARSING ROUTE
-                # SKIPPING ITEMS WE CANNOT PARSE
-                nums = re.findall(r'\d+', name)
-                if len(nums) == 0:
-                    print("[Y] ! Warning: Didn't find any numbers in item named '{}'\n >> Skipping item...".format(name))
-                    continue
-                if "￥" not in name:
-                    print("[Y] ! Warning: Didn't find YUAN in item named '{}'\n >> Skipping item...".format(name))
-                    continue
-                if name.count("￥") > len(nums):
-                    print("[Y] ! Warning: There are {} prices in item named '{}' but only {} numbers\n >> Skipping item...".format(name.count("￥"), name, len(nums)))
-                    continue
-                
-                # NAME PARSING
-                i["name"] = name.split("（")[0].split("(")[0].replace(".", "").replace("~", "").replace(",", "")
-                i["name"] = i["name"][i["name"].index(str(nums[0]))+len(str(nums[0])):]
+                    # BASE PRICE PARSING
+                    for x in range(name.count("￥")):
+                        i["basePrice"]["yuan"].append( int(nums[x]) )
+                        i["basePrice"]["euro"].append( round(i["basePrice"]["yuan"][x]/self.conversionRate["yuanToEuro"], 2) )
+                    
+                    # PRICE PARSING
+                    for basePrice in i["basePrice"]["euro"]:
+                        rounded = round(basePrice/5)*5
+                        i["price"].append( (rounded, rounded+(5*(math.floor(rounded/50) + 1))) )
+                    
+                    # TYPE PARSING (FULL DETECTION IS NOT REQUIRED WITH HUSKY REPS (hopefully))
+                    if len(i["type"]) > 1:
+                        i["type"] = i["type"][:name.count("￥")]
+                    
+                    print("[Y] DEBUG ITEM PARSING OUTPUT:\n{}".format(json.dumps(i, indent=2)))
+                    print("[Y] Parsed an {} {} selling for ￥ {}".format(i["brand"], i["type"], i["basePrice"]["yuan"]))
 
-                # DESCRIPTION PARSING
-                if "（" in name:
-                    i["desc"] = name.split("（")[1].replace("）", "")
-                elif "(" in name:
-                    i["desc"] = name.split("(")[1].replace(")", "")
-                else:
-                    i["desc"] = None
-                if i["desc"] is not None:
-                    if i["desc"][-4:] == "phot":
-                        i["desc"] += "o"
-                
-                # BRAND DECODING + PARSING
-                foundBrands = []
-                for brand in list(self.brands.keys()):
-                    if brand in i["name"]: # Brand found in decoding table
-                        foundBrands.append(brand)
-                        i["brand"].append(self.brands[brand])
-                        i["name"] = i["name"].replace(brand, "") # Remove the brand from the item name
-                
-                if len(foundBrands) < i["name"].upper().count(" X ") + 1: # If there were brands that were not found in the decoding table
-                    for namePart in i["name"].split(" X "):
-                        for idx in range(len(namePart)):
-                            if namePart[idx].isalpha():
-                                break
-                        if len(namePart) > 0:
-                            namePart = namePart[idx:].split(" ")[0]
-                            namePart = namePart[0].upper() + namePart[1:].lower()
-                            i["brand"].append(namePart)
-                
-                # NAME PARSING - REMOVING SPACES AFTER BRAND DECODING
-                while i["name"][0] == " ": # Remove additional spaces at the start
-                    i["name"] = i["name"][1:]
-                while i["name"][-1] == " ": # Remove additional spaces at the end
-                    i["name"] = i["name"][:-1]
-
-                # BASE PRICE PARSING
-                for x in range(name.count("￥")):
-                    i["basePrice"]["yuan"].append( int(nums[x]) )
-                    i["basePrice"]["euro"].append( round(i["basePrice"]["yuan"][x]/self.conversionRate["yuanToEuro"], 2) )
-                
-                # PRICE PARSING
-                for basePrice in i["basePrice"]["euro"]:
-                    rounded = round(basePrice/5)*5
-                    i["price"].append( (rounded, rounded+(5*(math.floor(rounded/50) + 1))) )
-                
-                # TYPE PARSING (FULL DETECTION IS NOT REQUIRED WITH HUSKY REPS (hopefully))
-                if len(i["type"]) > 1:
-                    i["type"] = i["type"][:name.count("￥")]
-                
-                print("[Y] DEBUG ITEM PARSING OUTPUT:\n{}".format(json.dumps(i, indent=2)))
-                print("[Y] Parsed an {} {} selling for ￥ {}".format(i["brand"], i["type"], i["basePrice"]["yuan"]))
-
-            parsed.append(i)
+                parsed.append(i)
+            except Exception as e:
+                print("[!!!] Unknown exception occured: {}\n >> Skipping item...".format(str(e)))
         return parsed
 
     def scrape(self, seller, url):
